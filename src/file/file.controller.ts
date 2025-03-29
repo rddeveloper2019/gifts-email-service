0310b98d79d02c94981fe219f76627f8e6672500
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-03-30 02:04:54 +0300

    create gift part-2

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Param, Res } from "@nestjs/common";
+import { Controller, Get, NotFoundException, Param, Res } from "@nestjs/common";
 import { createReadStream, existsSync, mkdirSync } from "fs";
 import { join } from "path";
 import { Response } from "express";
 
 const uploadPath = join(process.cwd(), "uploaded-files");
 
+if (!existsSync(uploadPath)) {
+  mkdirSync(uploadPath, { recursive: true });
+}
+
 @Controller("file")
 export class FileController {
   @Get(":id")
   getFile(@Res() res: Response, @Param("id") id: string) {
-    const file = createReadStream(join(uploadPath, id));
+    const path = join(uploadPath, id);
+    if (!existsSync(path)) {
+      throw new NotFoundException("File not found");
+    }
+    const file = createReadStream(path);
     file.pipe(res);
   }
 }
--- src/gifts/gifts.module.ts
+++ src/gifts/gifts.module.ts
@@ -4,8 +4,13 @@ import { CreateGiftProvider } from "./providers/create-gift.provider";
 import { GiftsController } from "./gifts.controller";
 import { FileSystemStoredFile, NestjsFormDataModule } from "nestjs-form-data";
 import { join } from "path";
+import { existsSync, mkdirSync } from "fs";
 
 const uploadPath = join(process.cwd(), "uploaded-files");
+
+if (!existsSync(uploadPath)) {
+  mkdirSync(uploadPath, { recursive: true });
+}
 
 @Module({
   imports: [
